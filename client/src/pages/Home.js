5a5c256a97a8ed5b640611cadb0cedee96305ad0
KJ Krueger <kjkrueger12@gmail.com>
2022-10-10 17:51:13 -0700

    updated welcome

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
 // import ThoughtList from '../components/ThoughtList';
 // import ThoughtForm from '../components/ThoughtForm';
@@ -15,13 +15,19 @@ const Home = () => {
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          style={{ border: "1px dotted #1a1a1a" }}
         >
           <h3>Welcome to custom made leather patches</h3>
-          <p>Add Description here</p>
+          <p>
+            Using only the best, top grain leather and made on our brand new XTool D1
+            Pro machine we can make just about anything you can dream. We mainly
+            stay with leather but we can also etch may other mediums such as
+            wood and can mark metal. If you can dream up an idea then we will do
+            our best to make that dream come true. Limited to a workspace of
+            32in x 32in
+          </p>
         </div>
-        <div className="col-12 col-md-8 mb-3">
-        </div>
+        <div className="col-12 col-md-8 mb-3"></div>
       </div>
     </main>
   );
